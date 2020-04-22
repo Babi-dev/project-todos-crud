317a846c8cae75b5cc7c78078e6e46ec38394684
Bárbara N. Sousa <53186630+Babi-dev@users.noreply.github.com>
2020-04-21 21:31:00 -0300

    initial filter structure

--- src/pages/TodoApp/TodoApp.jsx
+++ src/pages/TodoApp/TodoApp.jsx
@@ -3,15 +3,14 @@ import React from "react";
 import Header from "../../components/Header/Header";
 import TodoCreator from "./containers/TodoCreator/TodoCreator";
 import TodoList from "./containers/TodoList/TodoList";
+import TodoFilter from "./containers/TodoFilter/TodoFilter";
 
 function TodoApp() {
   return (
     <>
       <Header>
         <TodoCreator />
-        <select>
-          <option>all</option>
-        </select>
+        <TodoFilter />
       </Header>
       <TodoList />
       {/* <footer>Todo Footer</footer> */}
---
+++ src/pages/TodoApp/containers/TodoFilter/TodoFilter.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function TodoFilter() {
+  return (
+    <>
+      <select>
+        <option>all</option>
+      </select>
+    </>
+  );
+}
+
+export default TodoFilter;
